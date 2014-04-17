e1151749bfcb315595df197a5ed5d0e5d8786855
lihongzheshuai <wushikezuo@gmail.com>
2014-04-17 20:58:52 +0800

    ceshi

--- src/main/java/com/coderli/shurnim/storage/Main.java
+++ src/main/java/com/coderli/shurnim/storage/Main.java
@@ -1,5 +1,11 @@
 package com.coderli.shurnim.storage;
 
+/**
+ * 
+ * @author OneCoder
+ * @date 2014年4月17日 下午8:58:39
+ * @website http://www.coderli.com
+ */
 public class Main {
 
 	public static void main(String[] args) {
